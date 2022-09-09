1ff3db3e69d2a762d05b172295071d051dd4e56d
ajhiltonuk <ajhiltonuk@gmail.com>
2022-09-09 15:41:11 +0100

    new file:   ex10.py

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Alex Hilton'
+my_age = 42
+my_height = 90
+my_weight = 196
+my_eyes = 'blue'
+my_teeth = 'Yellow'
+my_hair = 'Bald'
+
+print(f'Lets talk about {my_name}.')
+print(f'Hes {my_height} inches tall')
+print(f'Hes {my_weight} pounds heavy.')
+print('Actually that is not too heavy')
+print(f'Hes got {my_eyes} eyes and {my_hair} hair')
+print(f'His teeth are usually {my_teeth} depending on the coffee.')
+
+total = my_age  + my_height + my_weight
+print(f'If I add {my_age}, {my_height}, and {my_weight} I get {total}.')
---
+++ ex9.py
@@ -0,0 +1,8 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""``
+Hellow""")
